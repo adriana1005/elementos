f648f83a15a236d3b3ec5dc20f175cf4869a81c5
adriana1005 <adrianaabreu1005@gmail.com>
2025-05-05 17:03:31 +0100

    modificações funções de valores em falta

--- valores_em_falta.py
+++ valores_em_falta.py
@@ -1,40 +1,29 @@
-from recolha import recolha
+from recolha import *
 
-ano, municipio, curso, ano_escolar, valor_alunos, valor_rsi, valor_gmm = recolha()
+df, ano, municipio, curso, ano_escolar, valor_alunos, valor_rsi, valor_gmm = recolha()
 
 def valores_em_falta(faltar):
+    '''funçao geral para todos os valores'''
     return faltar.isnull().sum()
 
 
-def faltar_ano(ano):
-    return valores_em_falta(ano)
-
-
-def faltar_regiao(municipio):
-    return valores_em_falta(municipio)
-
-
-def faltar_curso(curso):
-    return valores_em_falta(curso)
-
-
-def faltar_ano_escolar(ano_escolar):
-    return valores_em_falta(ano_escolar)
-
-
 def faltar_valores_alunos(valor_alunos):
+    '''valores em falta da desistência escolar em percentagem'''
+    print(faltar_valores_alunos(valor_alunos))
     return valores_em_falta(valor_alunos)
 
 
 def faltar_valor_rsi(valor_rsi):
+    '''valores em falta rendimento social de inserção'''
+    print(faltar_valor_rsi(valor_rsi))
     return valores_em_falta(valor_rsi)
 
 
 def faltar_valor_gmm(valor_gmm):
+    '''valores em falta do ganho médio mensal'''
+    print(faltar_valor_gmm(valor_gmm))
     return valores_em_falta(valor_gmm)
 
 
 
-print(faltar_valores_alunos(valor_alunos))
-print(faltar_valor_rsi(valor_rsi))
-print(faltar_valor_gmm(valor_gmm))
+
