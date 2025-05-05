ebbc2218d3d8bc498e189b6953508e4811b0765a
adriana1005 <adrianaabreu1005@gmail.com>
2025-05-05 18:08:43 +0100

    passar os valores da mediana para os valores em falta

--- recolha.py
+++ recolha.py
@@ -13,7 +13,8 @@ def recolha():
     valor_alunos = df['Valor Alunos'] #valor de desistência escolar em percentagem
     valor_rsi = df['Valor RSI '] #valor de pessoas que recebem o rendimento social de inserção
     valor_gmm = df['Valor GMM'] #valor em euros do ganho medio mensal
-
+    missing_values = df.isnull().sum()
+    print(missing_values)
 
     return df, ano, municipio, curso, ano_escolar, valor_alunos, valor_rsi, valor_gmm
 
--- valores_em_falta.py
+++ valores_em_falta.py
@@ -10,6 +10,14 @@ def valores_em_falta(faltar):
 def faltar_valores_alunos(valor_alunos):
     '''valores em falta da desistência escolar em percentagem'''
     print(faltar_valores_alunos(valor_alunos))
+
+    df = pd.read_csv('elementos.csv')
+
+    # Remover linhas com NaN na coluna 'email'
+    df.dropna(subset=['Valor Alunos'], inplace=True)
+
+    # Salvar o DataFrame modificado em um novo CSV
+    df.to_csv('dados_sem_nulos.csv', index=False)
     return valores_em_falta(valor_alunos)
 
 
@@ -23,7 +31,11 @@ def faltar_valor_gmm(valor_gmm):
     '''valores em falta do ganho médio mensal'''
     print(faltar_valor_gmm(valor_gmm))
     return valores_em_falta(valor_gmm)
+'''
+print(df.describe()) # para saber média, quartis, desvio padrão
 
+df.fillna(df.median(numeric_only=True), inplace=True)
 
-
+# Guardar o novo ficheiro CSV
+df.to_csv("dados_imputados.csv", index=False)'''
 
