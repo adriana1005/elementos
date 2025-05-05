ce8246bde0b3520a5f35db99cc67e85af016b7c6
adriana1005 <adrianaabreu1005@gmail.com>
2025-05-05 15:26:58 +0100

    funcao recolha de dados

---
+++ recolha.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+
+def recolha():
+    '''
+    separar os dados pelas várias variaveis
+    '''
+    df = pd.read_csv('elementoscsv.csv', sep=';', encoding='latin1')
+    print(df.columns)
+    ano = df['Ano'] #2012 a 2022
+    municipio = df['Regiao']
+    curso = df['Curso'] #Profissional ou Ensino Normal
+    ano_escolar = df['Ano Escolar'] #10 ao 12
+    valor_alunos = df['Valor Alunos'] #valor de desistência escolar em percentagem
+    valor_rsi = df['Valor RSI '] #valor de pessoas que recebem o rendimento social de inserção
+    valor_gmm = df['Valor GMM'] #valor em euros do ganho medio mensal
+    print(municipio)
+
+
+    return ano, municipio, curso, ano_escolar,valor_alunos, valor_rsi, valor_gmm
+
+
+recolha()
+
+
+
