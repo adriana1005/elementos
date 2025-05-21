81330074165a8e4c5b5166073d54dddb3828c8b2
adriana1005 <adrianaabreu1005@gmail.com>
2025-05-21 10:41:12 +0100

    mudancas

--- recolha.py
+++ recolha.py
@@ -4,13 +4,13 @@ def recolha():
     '''
     separar os dados pelas várias variaveis
     '''
-    df = pd.read_csv('dados_com_zscore1.csv', sep=';', encoding='latin1')
+    df = pd.read_csv('principal.csv', sep=';', encoding='latin1')
     ano = df['Ano'] #2012 a 2022
     municipio = df['Regiao']
     curso = df['Curso'] #Profissional ou Ensino Normal
     ano_escolar = df['Ano Escolar'] #10 ao 12
     valor_alunos = df['Valor Alunos'] #valor de desistência escolar em percentagem
-    valor_rsi = df['Valor RSI '] #valor de pessoas que recebem o rendimento social de inserção
+    valor_rsi = df['Valor RSI'] #valor de pessoas que recebem o rendimento social de inserção
     valor_gmm = df['Valor GMM'] #valor em euros do ganho medio mensal
     
     
